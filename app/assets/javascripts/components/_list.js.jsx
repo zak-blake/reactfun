8d890866a4c6c32fb23fbc80a4f7ce366968e2c5
zakary <94zakary@gmail.com>
2017-07-23 21:01:50 -0700

    removed intermediate lists component, added list deletion

--- app/assets/javascripts/components/_list.js.jsx
+++ app/assets/javascripts/components/_list.js.jsx
@@ -1,8 +1,14 @@
 var List = React.createClass({
+  handleClick(event) {
+    event.preventDefault();
+    this.props.deleteList(this.props.list.id);
+  },
+
   render() {
     return (
       <div className="list">
-        {this.props.list.name}
+        {this.props.list.name}<br />
+        <button onClick={this.handleClick}> Delete </button>
       </div>
     );
   }
--- app/assets/javascripts/components/_lists_container.js.jsx
+++ app/assets/javascripts/components/_lists_container.js.jsx
@@ -5,19 +5,47 @@ var ListsContainer = React.createClass({
   componentDidMount() {
     this.fetchLists();
   },
+
   fetchLists() {
     $.getJSON(
       this.props.listsPath,
       (data) => this.setState({lists: data})
     );
   },
+
+  deleteList(id) {
+    $.ajax({
+      url: `/lists/${id}`,
+      dataType: 'json',
+      type: 'DELETE',
+      context: this,
+      success(response) {
+
+        this.setState(state => {
+          var newLists = this.state.lists.filter((list) => {
+            return list.id != id;
+          });
+
+          return {lists: newLists};
+        });
+      }
+    });
+  },
+
   addList(newList) {
     this.setState({lists: this.state.lists.concat([newList])});
   },
+
   render() {
+    var lists = this.state.lists.map((list, listIndex) => {
+      return (
+        <List key={list.id} list={list} deleteList={this.deleteList} />
+      );
+    });
+
     return (
       <div>
-        <Lists lists={this.state.lists} />
+        {lists}
         <NewListForm addList={this.addList} />
       </div>
     );
